12961a8e448271eb8e6f4d4a7ffd3991198018c6
JFSilvaM <joaofrsima2000@gmail.com>
2026-08-14 13:00:26 +0200

    feat: add fonts and colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,27 @@
 import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 
+import { useFonts } from "expo-font";
 import "./global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
+  const [loaded, error] = useFonts({
+    "WorkSans-Black": require("@/assets/fonts/WorkSans-Black.ttf"),
+    "WorkSans-Light": require("@/assets/fonts/WorkSans-Light.ttf"),
+    "WorkSans-Medium": require("@/assets/fonts/WorkSans-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) return null;
+
   return <Slot />;
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <View className="mt-10">
-      <Text className="text-3xl font-bold text-blue-500">App</Text>
-    </View>
+    <SafeAreaView>
+      <View className="mt-6 mx-2.5">
+        <Text className="text-3xl font-work-black text-secondary-100">App</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
